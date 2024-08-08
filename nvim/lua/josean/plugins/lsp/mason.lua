c8a3665993417cc3263f1f279602114a7a9e112f
makinzm <nozomi.maki.da@gmail.com>
2024-08-08 09:03:30 +0900

    fix: bug (but it doent still work)

--- nvim/lua/josean/plugins/lsp/mason.lua
+++ nvim/lua/josean/plugins/lsp/mason.lua
@@ -1,54 +1,77 @@
-return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+    return
+end
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+    return
+end
 
-    local mason_tool_installer = require("mason-tool-installer")
+mason.setup()
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
-
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
+mason_lspconfig.setup({
+    ensure_installed = {
         "tsserver",
+        -- "gopls",
         "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
+        "biome",
+        -- "java_language_server",
+        "autotools_ls",
+        "pylyzer",
         "pyright",
-      },
-    })
+        "ruff",
+        "rust_analyzer",
+        "lua_ls",
+        "clangd",
+        "golangci_lint_ls",
+        "gopls",
+    }
+})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "eslint_d",
-      },
-    })
-  end,
+require("mason-lspconfig").setup_handlers {
+    function(server_name) 
+        require("lspconfig")[server_name].setup({})
+    end,
 }
+
+require("lspconfig").clangd.setup({
+    cmd = {
+        "/usr/bin/clangd-12",
+        "--header-insertion=never",
+        "--clang-tidy",
+        "--enable-config",
+        "-I/usr/include",
+        "-I/home/hope/workspace/atcoder_2/cpp/ac-library",
+    }
+})
+require("lspconfig").gopls.setup({})
+
+require("lspconfig").ruff.setup({})
+require("lspconfig").pyright.setup({
+    filetypes = { "python" },
+    root_dir = require("lspconfig/util").root_pattern(".venv"),
+    cmd = {
+        "bash",
+        "-c",
+        "source .venv/bin/activate && pyright-langserver --stdio"
+    }
+})
+require("lspconfig").lua_ls.setup({
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+        },
+    },
+})
+
+require("lspconfig").rust_analyzer.setup({})
+
+local keymap = vim.keymap
+
+-- enable keybinds for available lsp server
+local on_attach = function(cliend, bufnr)
+end
+
